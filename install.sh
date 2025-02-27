1ad90fd0a88fbd458b216a32b5135bb182b3b493
Alex Galati <153542834+alex-galati@users.noreply.github.com>
2025-02-27 10:21:50 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+if [[ -z "$1" ]]; then
+    wget https://raw.githubusercontent.com/alex-galati/dotfiles/refs/heads/master/.profile
+    wget https://raw.githubusercontent.com/alex-galati/dotfiles/refs/heads/master/.vimrc
+elif [[ -n "$1" && "$1" == "link" ]]; then
+    rm -f ~/.vimrc
+    ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
-rm -f ~/.vimrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-
-rm -f ~/.profile
-ln -s ~/.dotfiles/.profile ~/.profile
+    rm -f ~/.profile
+    ln -s ~/.dotfiles/.profile ~/.profile
+fi
